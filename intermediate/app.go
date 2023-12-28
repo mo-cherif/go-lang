4bcdb19b36c5b75dafaebe4180f80254ac1673c3
mo-cherif <mocherifh0@gmail.com>
2023-12-28 13:51:54 +0100

    feat: structs

--- intermediate/app.go
+++ intermediate/app.go
@@ -121,9 +121,59 @@ func FirstTurn(card1, card2, dealerCard string) string {
 	}
 }
 
+// TODO: define the 'Car' type struct
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
+}
+
+// TODO: define the 'Track' type struct
+type Track struct {
+	distance int
+}
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	battery := car.battery - car.batteryDrain
+	distance := car.speed
+	return Car{
+		battery:      battery,
+		distance:     distance,
+		batteryDrain: car.batteryDrain,
+		speed:        car.speed,
+	}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+
+	panic("Please implement the CanFinish function")
+}
+
 func main() {
-	fmt.Println(FirstTurn("ace", "ace", "jack"))
-	fmt.Println(FirstTurn("ace", "king", "ace"))
-	fmt.Println(FirstTurn("five", "queen", "ace"))
-	fmt.Println(FirstTurn("king", "ace", "queen"))
+	speed := 18
+	batteryDrain := 10
+	car := NewCar(speed, batteryDrain)
+	car = Drive(car)
+	fmt.Println(car)
+
 }
